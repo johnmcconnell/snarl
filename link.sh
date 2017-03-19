07e15123c04c25dd6b04849f81efa1adfe32a200
Heinz N. Gies <heinz@licenser.net>
2017-03-20 00:51:47 +0100

    Use env

---
+++ link.sh
@@ -0,0 +1,7 @@
+ln -s `pwd`/_build/prod/rel/snarl/lib /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/share /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/releases /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/etc /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/snmp /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/erts-8.2 /opt/local/fifo-snarl/
+ln -s `pwd`/_build/prod/rel/snarl/bin /opt/local/fifo-snarl/
